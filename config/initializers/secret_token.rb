ed05b39dbe69e4fefd8c8419a50c279b1b47ce39
Alex <lyosha85@gmail.com>
2013-09-17 18:50:52 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Charts::Application.config.secret_key_base = '35f9d0f77d6976b4977ddf744922261718809665d6b8417b807d384c5e377096e5e64e6b756d5a36cc61f461c9137415e5cc9e71d56c19c1e09aa54aaadb0662'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Charts::Application.config.session_store :cookie_store, key: '_charts_session'
